b34727f44be24bad81bff6b6240e376a7166030f
Owen Reynolds <owenreynolds4@gmail.com>
2022-09-20 17:00:12 -0400

    Test

---
+++ beta_macOS/beta_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  beta_macOSApp.swift
+//  beta_macOS
+//
+//  Created by Capstone on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct beta_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
